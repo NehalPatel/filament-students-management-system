4e0eb7024044cde11a6a8664d172abb34df7ecc4
Nehal Patel <iamnehalpatel@gmail.com>
2024-04-17 22:59:53 +0530

    New student panel created, with new guard.

--- app/Filament/Resources/StudentResource/Pages/EditStudent.php
+++ app/Filament/Resources/StudentResource/Pages/EditStudent.php
@@ -4,7 +4,10 @@ namespace App\Filament\Resources\StudentResource\Pages;
 
 use App\Filament\Resources\StudentResource;
 use Filament\Actions;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Hash;
 
 class EditStudent extends EditRecord
 {
@@ -13,6 +16,23 @@ class EditStudent extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('updatePassword')
+                ->form([
+                    TextInput::make('password')
+                        ->required()
+                        ->password()
+                        ->confirmed(),
+                    TextInput::make('password_confirmation')
+                        ->required()
+                        ->password(),
+                ])->action(function(array $data){
+                    $this->record->update(['password' => $data['password']]);
+
+                    Notification::make()
+                        ->title('Student Password Updated Successfully')
+                        ->success()
+                        ->send();
+                }),
             Actions\DeleteAction::make(),
         ];
     }
